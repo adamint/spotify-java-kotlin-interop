c7dbffd50a2046abf92869de40f06f0628d38675
Adam Ratzman <adam@adamratzman.com>
2021-04-09 08:35:46 -0600

    update interop example

--- src/main/java/com/adamratzman/SpotifyAlbumApiInterop.java
+++ src/main/java/com/adamratzman/SpotifyAlbumApiInterop.java
@@ -2,45 +2,15 @@ package com.adamratzman;
 
 import com.adamratzman.spotify.SpotifyApiBuilderKt;
 import com.adamratzman.spotify.SpotifyAppApi;
-import com.adamratzman.spotify.javainterop.SpotifyContinuation;
-import com.adamratzman.spotify.models.Album;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.concurrent.ExecutionException;
 
 public class SpotifyAlbumApiInterop {
     static SpotifyAppApi api;
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException {
-        SpotifyApiBuilderKt.spotifyAppApi(Const.clientId, Const.clientSecret).build(true, new SpotifyContinuation<>() {
-            @Override
-            public void onSuccess(SpotifyAppApi spotifyAppApi) {
-                api = spotifyAppApi;
-                runAlbumSearch();
-            }
-
-            @Override
-            public void onFailure(@NotNull Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        });
-
-        Thread.sleep(1000000);
-    }
-
-    public static void runAlbumSearch() {
-        api.getAlbums().getAlbum("spotify:album:0b23AHutIA1BOW0u1dZ6wM", null, new SpotifyContinuation<>() {
-            @Override
-            public void onSuccess(Album album) {
-                System.out.println("Album name is: " + album.getName());
-
-                System.exit(0);
-            }
-
-            @Override
-            public void onFailure(@NotNull Throwable throwable) {
-                throwable.printStackTrace();
-            }
-        });
+    public static void main(String[] args) {
+        var api = SpotifyApiBuilderKt.spotifyAppApi(Const.clientId, Const.clientSecret).buildRestAction(true).complete();
+        var album = api.getAlbums().getAlbumRestAction("spotify:album:0b23AHutIA1BOW0u1dZ6wM", null).complete();
+        System.out.println("Album name is: " + album.getName());
     }
 }
--- src/main/java/com/adamratzman/SpotifyBrowseApiInterop.java
+++ src/main/java/com/adamratzman/SpotifyBrowseApiInterop.java
@@ -1,42 +1,25 @@
 package com.adamratzman;
 
 import com.adamratzman.spotify.SpotifyApiBuilderKt;
-import com.adamratzman.spotify.SpotifyAppApi;
-import com.adamratzman.spotify.javainterop.SpotifyContinuation;
-import com.adamratzman.spotify.models.PagingObject;
 import com.adamratzman.spotify.models.SimpleAlbum;
-import org.jetbrains.annotations.NotNull;
+import com.adamratzman.spotify.models.SpotifyCategory;
+import kotlin.Unit;
 
 public class SpotifyBrowseApiInterop {
-    static SpotifyAppApi api;
-
     public static void main(String[] args) throws InterruptedException {
-        SpotifyApiBuilderKt.spotifyAppApi(Const.clientId, Const.clientSecret).build(true, new SpotifyContinuation<>() {
-            @Override
-            public void onSuccess(SpotifyAppApi spotifyAppApi) {
-                api = spotifyAppApi;
-
-                api.getBrowse().getNewReleases(20, null, null, new SpotifyContinuation<>() {
-                    @Override
-                    public void onSuccess(PagingObject<SimpleAlbum> simpleAlbums) {
-                            for (SimpleAlbum album : simpleAlbums) {
-                                System.out.println(album.getName());
-                            }
-                    }
-
-                    @Override
-                    public void onFailure(@NotNull Throwable throwable) {
-
-                    }
-                });
-            }
-
-            @Override
-            public void onFailure(@NotNull Throwable throwable) {
-                throwable.printStackTrace();
-            }
+        var api = SpotifyApiBuilderKt.spotifyAppApi(Const.clientId, Const.clientSecret).buildRestAction(true).complete();
+        var browseReleases = api.getBrowse().getNewReleasesRestAction(20, null, null).complete();
+        for (SimpleAlbum album : browseReleases.getItems()) {
+            System.out.println(album.getName());
+        }
+        api.getBrowse().getCategoryListRestAction(20, null, null, null).queue(throwable -> {
+            throwable.printStackTrace();
+            return Unit.INSTANCE;
+        }, spotifyCategories -> {
+            for (SpotifyCategory category : spotifyCategories.getItems()) System.out.println(category.getName());
+            return Unit.INSTANCE;
         });
 
-        Thread.sleep(1000000);
+        Thread.sleep(3000);
     }
 }
