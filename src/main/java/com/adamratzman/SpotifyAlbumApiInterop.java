afb02addb225b3ecc0d445585da9cbdcf0dac7e2
Adam Ratzman <adam@adamratzman.com>
2021-04-14 15:45:46 -0400

    update web server to use rest actions

--- src/main/java/com/adamratzman/SpotifyAlbumApiInterop.java
+++ src/main/java/com/adamratzman/SpotifyAlbumApiInterop.java
@@ -2,6 +2,8 @@ package com.adamratzman;
 
 import com.adamratzman.spotify.SpotifyApiBuilderKt;
 import com.adamratzman.spotify.SpotifyAppApi;
+import com.adamratzman.spotify.SpotifyClientApi;
+import com.adamratzman.spotify.SpotifyUserAuthorization;
 import com.adamratzman.spotify.models.Album;
 
 import java.util.concurrent.ExecutionException;
@@ -10,7 +12,8 @@ public class SpotifyAlbumApiInterop {
     static SpotifyAppApi api;
 
     public static void main(String[] args) {
-        SpotifyAppApi api = SpotifyApiBuilderKt.spotifyAppApi(Const.clientId, Const.clientSecret).buildRestAction(true).complete();
+        SpotifyClientApi api = SpotifyApiBuilderKt.spotifyClientApi(Const.clientId, Const.clientSecret, null,
+                new SpotifyUserAuthorization(null, "", null, null, null), spotifyApiOptions -> null).buildRestAction(true).complete();
         Album album = api.getAlbums().getAlbumRestAction("spotify:album:0b23AHutIA1BOW0u1dZ6wM", null).complete();
         System.out.println("Album name is: " + album.getName());
     }
--- src/main/java/com/adamratzman/webserverauth/SpotifyWebClientAuthServer.java
+++ src/main/java/com/adamratzman/webserverauth/SpotifyWebClientAuthServer.java
@@ -5,23 +5,22 @@ import com.adamratzman.spotify.SpotifyApiBuilderKt;
 import com.adamratzman.spotify.SpotifyClientApi;
 import com.adamratzman.spotify.SpotifyScope;
 import com.adamratzman.spotify.SpotifyUserAuthorization;
-import com.adamratzman.spotify.javainterop.SpotifyContinuation;
 import com.adamratzman.spotify.models.PagingObject;
 import com.adamratzman.spotify.models.SimplePlaylist;
-import org.jetbrains.annotations.NotNull;
 import spark.ModelAndView;
+import spark.Request;
+import spark.Response;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 import static spark.Spark.get;
 import static spark.Spark.port;
 
 public class SpotifyWebClientAuthServer {
-    private static String redirectUri = "http://localhost/spotify-callback";
-    private static String authorizationUrl = SpotifyApiBuilderKt.getSpotifyAuthorizationUrl(
+    private static final String redirectUri = "http://localhost/spotify-callback";
+    private static final String authorizationUrl = SpotifyApiBuilderKt.getSpotifyAuthorizationUrl(
             new SpotifyScope[]{SpotifyScope.PLAYLIST_READ_PRIVATE, SpotifyScope.PLAYLIST_MODIFY_PRIVATE},
             Const.clientId,
             redirectUri,
@@ -30,82 +29,62 @@ public class SpotifyWebClientAuthServer {
             null
     );
 
-   /* public static void main(String[] args) {
+    public static void main(String[] args) {
         port(80);
 
-        get("/", (request, response) -> {
-            Map<String, Object> model = new HashMap<>();
-            Object apiObject = request.session().attribute("api");
-            model.put("api", apiObject);
-            return new ModelAndView(model, "index.hbs");
-        }, new HandlebarsTemplateEngine());
+        get("/", SpotifyWebClientAuthServer::handleHomeRequest, new HandlebarsTemplateEngine());
+        get("/submit-playlist-for-modifications", SpotifyWebClientAuthServer::handleModifyPlaylistRequest, new HandlebarsTemplateEngine());
+        get("/redirect-to-auth", SpotifyWebClientAuthServer::handleAuthRedirectRequest);
 
-        get("/submit-playlist-for-modifications", (request, response) -> {
-            String playlistName = request.queryParams("playlistName");
-            String valenceLevelString = request.queryParams("valenceLevel");
-            Map<String, Object> model = new HashMap<>();
+        get("/spotify-callback", SpotifyWebClientAuthServer::handleSpotifyCallbackRequest);
+    }
+
+    private static ModelAndView handleHomeRequest(Request request, Response response) {
+        Map<String, Object> model = new HashMap<>();
+        Object apiObject = request.session().attribute("api");
+        model.put("api", apiObject);
+        return new ModelAndView(model, "index.hbs");
+    }
+
+    private static ModelAndView handleModifyPlaylistRequest(Request request, Response response) {
+        String playlistName = request.queryParams("playlistName");
+        String valenceLevelString = request.queryParams("valenceLevel");
+        Map<String, Object> model = new HashMap<>();
+        try {
+            float valenceLevel = Float.parseFloat(valenceLevelString);
+            SpotifyClientApi api = request.session().attribute("api");
+            PagingObject<SimplePlaylist> clientPlaylists = api.getPlaylists().getClientPlaylistsRestAction(50, 0).complete();
+
+        } catch (Exception ignored) { // i'm lazy
+            model.put("success", false);
+        }
+
+        System.out.println(request.queryParams());
+        return new ModelAndView(model, "modification.hbs");
+    }
+
+    private static Object handleAuthRedirectRequest(Request request, Response response) {
+        response.redirect(authorizationUrl);
+        return null;
+    }
+
+    private static Object handleSpotifyCallbackRequest(Request request, Response response) {
+        if (request.queryParams("code") == null) response.redirect(authorizationUrl);
+        else {
             try {
-                float valenceLevel = Float.parseFloat(valenceLevelString);
-                SpotifyClientApi api = request.session().attribute("api");
-                AtomicBoolean finished = new AtomicBoolean(false);
-                api.getPlaylists().getClientPlaylists(50, 0, new SpotifyContinuation<>() {
-                    @Override
-                    public void onSuccess(PagingObject<SimplePlaylist> simplePlaylists) {
-
-                    }
-
-                    @Override
-                    public void onFailure(@NotNull Throwable throwable) {
-                        throwable.printStackTrace();
-                        finished.set(true);
-                    }
-                });
-                while (!finished.get()) {
-                }
-
-            } catch (Exception ignored) { // i'm lazy
-                model.put("success", false);
-            }
-
-            System.out.println(request.queryParams());
-            return new ModelAndView(model, "modification.hbs");
-        }, new HandlebarsTemplateEngine());
-
-        get("/redirect-to-auth", (request, response) -> {
-            response.redirect(authorizationUrl);
-            return null;
-        });
-
-        get("/spotify-callback", (request, response) -> {
-            if (request.queryParams("code") == null) response.redirect(authorizationUrl);
-            else {
-                AtomicBoolean loaded = new AtomicBoolean(false);
-                AtomicBoolean success = new AtomicBoolean(false);
                 String code = request.queryParams("code");
-                SpotifyApiBuilderKt.spotifyClientApi(Const.clientId,
+                SpotifyClientApi spotifyClientApi = SpotifyApiBuilderKt.spotifyClientApi(Const.clientId,
                         Const.clientSecret,
                         redirectUri,
                         new SpotifyUserAuthorization(code, null, null, null, null),
-                        spotifyApiOptions -> null).build(true, new SpotifyContinuation<>() {
-                    @Override
-                    public void onSuccess(SpotifyClientApi spotifyClientApi) {
-                        request.session().attribute("api", spotifyClientApi);
-                        loaded.set(true);
-                        success.set(true);
-                    }
+                        spotifyApiOptions -> null).buildRestAction(true).complete();
 
-                    @Override
-                    public void onFailure(@NotNull Throwable throwable) {
-                        loaded.set(true);
-                        success.set(false);
-                    }
-                });
-                while (!loaded.get()) {
-                }
-                if (success.get()) response.redirect("/");
-                else response.redirect(redirectUri);
+                request.session().attribute("api", spotifyClientApi);
+                response.redirect("/");
+            } catch (Exception e) {
+                response.redirect(authorizationUrl);
             }
-            return null;
-        });
-    }*/
+        }
+        return null;
+    }
 }
